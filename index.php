f4734f9922bcb9dde270e529b609ca1e5bf21ef2
mtomberlin <mtomberlin@csumb.edu>
2017-09-13 22:07:52 +0000

    PHP function and HTML template

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include 'inc/functions.php';
+?>
+
+<html>
+    <head>
+        <title> Silverjack </title>
+        <meta charset="utf-8"/>
+        <style>
+            @import url("css/styles.css");
+        </style>
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Tammudu" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Silverjack</h1>
+        <div id="main">
+            <?php
+                play();
+            ?>
+            <FORM>
+                <INPUT TYPE="button" onClick="history.go(0)" VALUE="Play Again">
+            </FORM>
+        </div>
+    </body>
+</html>
